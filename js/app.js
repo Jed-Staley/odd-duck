9221b6a2b6728185ec48a4460745dbbf0fb936f4
Jed Staley <JNStaley1@gmail.com>
2024-03-06 16:10:36 -0500

    Added local storage implementation and updated README

--- js/app.js
+++ js/app.js
@@ -3,35 +3,41 @@
 let showNImages = 3;
 let collectNVotes = 25;
 
-let products = [];
+let products = JSON.parse(localStorage.getItem('products')) || [];
 
 function Product(name) {
   this.name = name;
   this.src = 'images/' + name + '.jpg';
   this.votes = 0;
   this.views = 0;
+  let color1 = Math.floor(256 * Math.random());
+  let color2 = Math.floor(256 * Math.random());
+  let color3 = Math.floor(256 * Math.random());
+  this.color = `rgba(${color1}, ${color2}, ${color3}, 1)`;
   products.push(this);
 }
 
-new Product('bag');
-new Product('banana');
-new Product('bathroom');
-new Product('boots');
-new Product('breakfast');
-new Product('bubblegum');
-new Product('chair');
-new Product('cthulhu');
-new Product('dog-duck');
-new Product('dragon');
-new Product('pen');
-new Product('pet-sweep');
-new Product('scissors');
-new Product('shark');
-new Product('sweep');
-new Product('tauntaun');
-new Product('unicorn');
-new Product('water-can');
-new Product('wine-glass');
+if (products[0] === undefined) {
+  new Product('bag');
+  new Product('banana');
+  new Product('bathroom');
+  new Product('boots');
+  new Product('breakfast');
+  new Product('bubblegum');
+  new Product('chair');
+  new Product('cthulhu');
+  new Product('dog-duck');
+  new Product('dragon');
+  new Product('pen');
+  new Product('pet-sweep');
+  new Product('scissors');
+  new Product('shark');
+  new Product('sweep');
+  new Product('tauntaun');
+  new Product('unicorn');
+  new Product('water-can');
+  new Product('wine-glass');
+}
 
 function loadPictureScheme() {
   const imagesSection = document.getElementById('images');
@@ -77,6 +83,7 @@ function loadNewProducts() {
       if (voteCounter === collectNVotes) {
         endVoting();
       } else {
+        localStorage.setItem('products', JSON.stringify(products));
         loadNewProducts();
       }
     };
@@ -113,15 +120,12 @@ function makeCharts() {
   let namesArray = [];
   let viewsArray = [];
   let votesArray = [];
-  let chartColors = [];
+  let colorsArray = [];
   for (let i = 0; i < products.length; i++) {
     namesArray.push(products[i].name);
     viewsArray.push(products[i].views);
     votesArray.push(products[i].votes);
-    let color1 = Math.floor(256 * Math.random());
-    let color2 = Math.floor(256 * Math.random());
-    let color3 = Math.floor(256 * Math.random());
-    chartColors.push(`rgba(${color1}, ${color2}, ${color3}, 1)`);
+    colorsArray.push(products[i].color);
   }
 
   function makeBarChart(canvasid, data, labels, chartLabel) {
@@ -134,7 +138,7 @@ function makeCharts() {
         datasets: [{
           label: '# of ' + chartLabel,
           data: data,
-          backgroundColor: chartColors,
+          backgroundColor: colorsArray,
           borderWidth: 1,
           text: 'white'
         }]
